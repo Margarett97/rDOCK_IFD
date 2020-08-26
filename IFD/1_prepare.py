93be2399289ef56ac95ed2ac541dd8d6253b8704
Margarett97 <pukmalgorzata24@gmail.com>
2020-08-26 11:27:11 +0200

    1

---
+++ IFD/1_prepare.py
@@ -0,0 +1,23 @@
+from parameters import *
+import os
+
+os.chdir(direct)
+
+with open(receptor + ".pdb", "r") as f:        # ligand selection from receptor file
+    line = f.readlines()
+    with open(rec_ligand + ".pdb" , "w") as n:
+        for i, lines in enumerate(line):
+            if lines.startswith("HETATM"):
+                a = lines.split()
+                if a[3] == rec_ligand:
+                    n.write(lines)
+n.close()
+
+with open(receptor + ".pdb", "r") as f:        # receptor selection
+    line = f.readlines()              
+    with open(receptor+ "_prep.pdb" , "w") as n:
+        for i, lines in enumerate(line):
+            if lines.startswith("ATOM"):
+                n.write(lines)
+n.close()
+                
---
+++ IFD/parameters.py
@@ -0,0 +1,7 @@
+direct = "/home/malgorzata/Desktop/rDOCK_IFD/Wyniki/1_aldose_rductase"
+
+receptor = "2acr"
+
+rec_ligand = "NAP"
+
+ligand = "TOL"
